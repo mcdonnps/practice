22c9af93c9fe6d8a6ad489a16d1979b0f4457382
petebell3 <pete.bellisano@gmail.com>
2014-03-18 09:36:44 -0400

    homework

--- test.rb
+++ test.rb
@@ -1,1 +1,14 @@
-# Empty for now
+#3-18-14
+#
+#
+puts 'Please enter your name:'
+name = gets.chomp
+puts 'Hi ' + name + '!'
+if name == 'Joe'
+  puts 'Sup my dude!'
+else
+puts 'Please allow me to configure your age in years to seconds.'
+end
+puts 'What is your current age in years please?'
+age.to_i= gets.chomp
+puts 'Your age in seconds is ' + age.to_i*(31536000) + '!'
